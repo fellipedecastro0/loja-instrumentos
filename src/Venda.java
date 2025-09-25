1ca294be92d60a082cb42aaceb94eb799880f3b1
FellipeDCastroo <fellipe.castro@sempreceub.com>
2025-09-25 13:03:55 -0300

    feat: Implementa as classes ItemVenda and Venda

--- src/Venda.java
+++ src/Venda.java
@@ -1,8 +1,35 @@
 import java.util.Date;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Venda {
     private int id;
     private Date data;
-    private list<ItemVenda> itens;
-    private double ValorTotal;
+    private List<ItemVenda> itens;
+    private double valorTotal;
+
+    public Venda(int id, Date data){
+        this.id = id;
+        this.data = data;
+        this.valorTotal = 0.0;
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarProduto(Produto p, int qtd) {
+        ItemVenda novoItem = new ItemVenda(p, qtd);
+        this.itens.add(novoItem);
+        this.valorTotal = calcularTotal();
+    }
+    public double calcularTotal() {
+        double somaTotal = 0.0;
+
+        for (ItemVenda item : this.itens) {
+            somaTotal += item.getSubtotal();
+        }
+
+        return somaTotal;
+    }
+
+    public double getValorTotal() {
+        return this.valorTotal;
+    }
 }
